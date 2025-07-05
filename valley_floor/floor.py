7bcdb9e4cf078f1b2c3f827f46cd52a54c483328
Arthur Koehl <avkoehl@ucdavis.edu>
2025-07-04 18:56:19 -0700

    code for combining outputs

---
+++ valley_floor/floor.py
@@ -0,0 +1,24 @@
+"""
+
+flood_extent floor + low slope connectivity floor = valley floor
+
+post process combined and label
+
+Burnin streams
+Close holes below certain size
+Optional label by subbasin
+Optional Convert to polygon
+
+
+"""
+
+
+def combine_floors(
+    flood_extent_floor,
+    low_slope_floor,
+    channel_network,
+    max_fill_area,
+    subbasin=None,
+    output_vector=False,
+):
+    pass
