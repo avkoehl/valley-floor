9d9f8c9df873a1bcc29b5f74d9a87a70f8902254
Arthur Koehl <avkoehl@ucdavis.edu>
2025-07-06 16:53:43 -0700

    move outside this package

--- valley_floor/utils.py
+++ valley_floor/utils.py
@@ -1,38 +1,5 @@
 import numpy as np
-from scipy.ndimage import gaussian_filter
 from skimage.morphology import label
-
-
-def filter_nan_gaussian_conserving(arr, spatial_radius, resolution, sigma):
-    """Apply a gaussian filter to an array with nans.
-    modified from:
-    https://stackoverflow.com/a/61481246
-
-    Intensity is only shifted between not-nan pixels and is hence conserved.
-    The intensity redistribution with respect to each single point
-    is done by the weights of available pixels according
-    to a gaussian distribution.
-    All nans in arr, stay nans in gauss.
-    """
-    radius_pixels = int(round(spatial_radius / resolution))
-    nan_msk = np.isnan(arr)
-
-    loss = np.zeros(arr.shape)
-    loss[nan_msk] = 1
-    loss = gaussian_filter(
-        loss, sigma=sigma, mode="constant", cval=1, radius=radius_pixels
-    )
-
-    gauss = arr.copy()
-    gauss[nan_msk] = 0
-    gauss = gaussian_filter(
-        gauss, sigma=sigma, mode="constant", cval=0, radius=radius_pixels
-    )
-    gauss[nan_msk] = np.nan
-
-    gauss += loss * arr
-
-    return gauss
 
 
 def connected(binary, flowpaths):
