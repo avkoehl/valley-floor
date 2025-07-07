f4fb72f20d94f4f90593a6cfc39ab29628e046f1
Arthur Koehl <avkoehl@ucdavis.edu>
2025-07-07 14:31:15 -0700

    post process methods tested

--- valley_floor/postprocess.py
+++ valley_floor/postprocess.py
@@ -2,42 +2,49 @@
 This module provides functions to post-process a floor raster.
 """
 
+from skimage.morphology import remove_small_holes
+
 
 def combine_floors(
     flood_extent_floor,
     low_slope_floor,
 ):
-    pass
+    return flood_extent_floor | low_slope_floor
 
 
 def burnin_streams(
     floor,
     channel_network,
 ):
-    pass
+    return floor + (channel_network > 0)
 
 
 def close_holes(
     floor,
-    min_size,
+    max_fill_area,
 ):
-    pass
+    filled = floor.copy(deep=True)
+    num_cells = max_fill_area / floor.rio.resolution()[0] ** 2
+    filled.data = remove_small_holes(filled.data, num_cells)
+    return filled
 
 
 def label_by_subbasin(
     floor,
     subbasins,
 ):
-    pass
+    return (floor > 0) * subbasins
 
 
 def convert_to_polygon(
     floor,
 ):
+    # TODO
     pass
 
 
 def remove_boundary_slopes(floor, slope_threshold):
+    # TODO
     pass
 
 
@@ -45,15 +52,10 @@ def process_floor(
     flood_extent_floor,
     low_slope_floor,
     channel_network,
-    slope_threshold=None,
     min_size=0,
     subbasins=None,
-    return_polygon=False,
 ):
     floor = combine_floors(flood_extent_floor, low_slope_floor)
-
-    if slope_threshold is not None:
-        floor = remove_boundary_slopes(floor, slope_threshold)
 
     floor = burnin_streams(floor, channel_network)
 
@@ -63,7 +65,7 @@ def process_floor(
     if subbasins is not None:
         floor = label_by_subbasin(floor, subbasins)
 
-    if return_polygon:
-        floor = convert_to_polygon(floor)
+    # if return_polygon:
+    #    floor = convert_to_polygon(floor)
 
     return floor
